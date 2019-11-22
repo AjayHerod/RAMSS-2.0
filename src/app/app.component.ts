aa5f54fb30b40a55117ffa46f6622e860768d131
Tony Shao <tonyshaocs@gmail.com>
2019-11-22 01:47:42 -0500

    move to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public accManage: any = true;
   title = 'RAMSS2';
+  
+  toggleAcc():any{
+	if (this.accManage == true){
+		this.accManage = false;
+	}
+	else{
+		this.accManage = true;
+	}
+  }
+  
+  
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AccInfoComponent } from './acc-info/acc-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     AccInfoComponent
   ],
   imports: [
