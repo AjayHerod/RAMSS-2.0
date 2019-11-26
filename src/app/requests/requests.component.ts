5a56dafc9059289b47752402ff99b267d08a2b47
Tony Shao <tonyshaocs@gmail.com>
2019-11-26 02:46:30 -0500

    add info for requests #8

--- src/app/requests/requests.component.ts
+++ src/app/requests/requests.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestsComponent implements OnInit {
 
+  public infoBody = "Select a request to learn more.";
   constructor() { }
-
   ngOnInit() {
+  }
+  
+  setInfo1(){
+  }
+  setInfo2(){
+	this.infoHeader = "Proof of Enrolment Letter - Verifies that you are currently or were previously enrolled in a program.";
+	this.infoBody = "This letter can be used for RESP purposes. \n\
+				Confirm a Current Student – confirms your enrolment status for the current term or academic year. If you are currently enrolled in courses for the Fall term only, your enrolment will only be confirmed for the Fall. For degree program students, we will confirm enrolment in course intentions if available on your Ryerson record.\
+				Confirm you were enrolled at Ryerson – this confirms you were previously a student at Ryerson, program or courses and years of attendance.";
+    this.infoFooter = "Charge per Letter: $20 per letter, additional copies $5";
+  }
+  
+  setInfo4(){
+	this.infoHeader = "Eligibility to Graduate Letter - Verifies that you have met all of your graduation requirements for your program although you have not yet officially graduated.";
+	this.infoBody = "You should be in your final year/semester of study, have applied/will be applying to graduate. ";
+	this.infoFooter = "Charge per Letter: $20 per letter, additional copies $5";
+  }
+  
+  setInfo5(){
+	this.infoHeader = "Jury Duty Letter - Current students must login to their Student Center via RAMSS to request a letter verifying student status, for exemption from Jury Duty. "
   }
 
 }
