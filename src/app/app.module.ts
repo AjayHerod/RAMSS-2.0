951d30e2e14427ca150c57c153f6bc17d5cabf52
Maitreya Meshram <mm360x@gmail.com>
2019-11-26 19:05:31 -0500

    Added sysfunction schedular #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import * as $ from 'jquery';
 import * as bootstrap from 'bootstrap';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { ScheduleModule, RecurrenceEditorModule } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
 	HttpClientModule,
     AppRoutingModule,
-	FormsModule  
+	FormsModule,
+	ScheduleModule, RecurrenceEditorModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
