6546ab856db209ccf9f906967cd66a6e546b0d14
Tony Shao <tonyshaocs@gmail.com>
2019-11-26 20:47:39 -0500

    add ui for managing courses #3

--- src/app/manage-courses/manage-courses.component.ts
+++ src/app/manage-courses/manage-courses.component.ts
@@ -9,7 +9,29 @@ export class ManageCoursesComponent implements OnInit {
 
   constructor() { }
 
+  public fallYear:number;
+  public winterYear:number;
+  public summerYear:number;
+
   ngOnInit() {
+	this.fallYear = 2019;
+	this.winterYear = 2020;
+	this.summerYear = 2020;
+  }
+
+  chooseFall(){
+	$(".btn").removeClass("active");
+    $('#fallButton').addClass('active');
+  }
+
+  chooseWinter(){
+	$(".btn").removeClass("active");
+    $('#winterButton').addClass('active');
+  }
+
+  chooseSummer(){
+	$(".btn").removeClass("active");
+    $('#summerButton').addClass('active');
   }
 
 }
