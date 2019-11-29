86baf605f4084950b1a02d519924d0d192870cb0
mantaev-vfl <ali_mantaev@hotmail.com>
2019-11-29 00:09:59 -0500

    update #7
    working on the calculations for advisement report

--- src/app/academic/academic.component.ts
+++ src/app/academic/academic.component.ts
@@ -7,19 +7,37 @@ import { course } from '../course-sum/course';
   styleUrls: ['./academic.component.css']
 })
 export class AcademicComponent implements OnInit {
-	constructor() { }
 
-					/* Course code, grade */
+
+	public allCourses: string[] = new Array();
+						/* Course code, grade */
 	public cCourses: [string, string][] = new Array();
 	public prCourses: [string, string][] = new Array();
 	public oeCourses: [string, string][] = new Array();
 	public laCourses: [string, string][] = new Array();
 	public lbCourses: [string, string][] = new Array();
-	//public otherCourses: [string, string][] = new Array(); 
+	
+	
+	public busCourses: [string, string][] = new Array();
+	public mthCourses: [string, string][] = new Array();
+	public cpsCourses: [string, string][] = new Array();
+
+
+	public cProgress: string;
+	public prProgress: string;
+	public oeProgress: string;
+	public laProgress: string;
+	public lbProgress: string;
+	public bProgress: string;
+	public mthProgress: string;
+
+
+	constructor() { }
+
 
 	ngOnInit() {
-		this.loadSchedule();
 		this.loadGrades();
+		this.loadSchedule();
 	} 
 		
 
@@ -29,8 +47,10 @@ export class AcademicComponent implements OnInit {
 			url: '/loadGrades',
 			contentType: 'application/json',
 			success: (data) => {
-				console.log(data);
 				data.forEach(course => {
+					this.allCourses.push(course.Course);
+
+					// based on course type
 					switch(course.Type) {
 						case "R":
 							this.cCourses.push([course.Course, course.Grade]);
@@ -60,12 +80,31 @@ export class AcademicComponent implements OnInit {
 							break;
 					}
 
+					// specific types of courses (subset of the professionally-related)
+					switch (course.Course.substring(0, 3)) {
+						case "MTH":
+							this.mthCourses.push([course.Course, course.Grade]);
+							break;
+						
+						case "ACC":
+							this.busCourses.push([course.Course, course.grade]);
+							break;
+						
+						case "CPS":
+							this.cpsCourses.push([course.Course, course.grade]);
+							break;
+					}
+
 				});
-				console.log(this.cCourses);
+				/*console.log(this.cCourses);
 				console.log(this.prCourses);
 				console.log(this.laCourses);
 				console.log(this.lbCourses);
 				console.log(this.oeCourses);
+				console.log(this.bCourses);*/
+				console.log(this.mthCourses);
+				console.log(this.busCourses);
+				console.log(this.cpsCourses);
 
 			},
 			error: function() {
@@ -81,12 +120,34 @@ export class AcademicComponent implements OnInit {
 			url: '/loadCalendar',
 			contentType: 'application/json',
 			success: (data) => {
-				//console.log(data);
+				console.log(data);
+				let requiredAmount = data[0];
+				this.bProgress = this.evaluateProgress(1, 3, this.busCourses);
+				this.laProgress = this.evaluateProgress(requiredAmount.LiberalA, requiredAmount.LiberalA, this.laCourses);
+				this.lbProgress = this.evaluateProgress(requiredAmount.LiberalB, requiredAmount.LiberalB, this.lbCourses);
+				this.oeProgress = this.evaluateProgress(requiredAmount.Open, requiredAmount.Open, this.oeCourses);
+				this.prProgress = this.evaluateProgress(5, 7, this.prCourses);
+				this.cProgress = this.evaluateProgress(requiredAmount.Required.length, requiredAmount.Required.length, this.cCourses);
+				// do the rest here: mth, cps, bus, etc.
+				
+
 			},
 			error: function() {
 				console.log("Failed to connect to server");
 			}
 		 })
+	}
+
+	evaluateProgress(min: number, max: number, coursesTaken: [string, string][]) {
+		if (coursesTaken.length < min) {
+			return min + " required, " + (min-coursesTaken.length) + " needed";
+		}
+		else if (coursesTaken.length > max) {
+			return "You have exceeded the maximum number";	
+		}
+		else {
+			return min + "required, " + coursesTaken.length + " completed";
+		}
 	}
   
 }
